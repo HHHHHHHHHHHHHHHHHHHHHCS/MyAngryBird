f6c1daa67cb3b79641619dd313f701b21797758e
HCS <464962683@qq.com>
2018-03-07 23:03:28 +0800

    小鸟跟随鼠标拉伸

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+
+    private Camera mainCamera;
+    private bool isClick = false;
+
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void OnMouseDown()
+    {
+        isClick = true;
+    }
+
+
+    private void OnMouseUp()
+    {
+        isClick = false;
+    }
+
+    private void Update()
+    {
+        if (isClick)
+        {
+            transform.position = mainCamera.ScreenToWorldPoint(Input.mousePosition+Vector3.forward);
+        }
+    }
+}
