37071fed4712baa2836ca621514d9a250d1ff90e
HCS <464962683@qq.com>
2018-03-08 22:50:50 +0800

    小鸟飞行

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -4,31 +4,54 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
+    private const float maxDistance = 1.25f;
 
     private Camera mainCamera;
+    private Transform rightBranchPos;
     private bool isClick = false;
+    private Rigidbody2D rigi;
+    private SpringJoint2D springJoint;
 
     private void Awake()
     {
         mainCamera = Camera.main;
+        rigi = GetComponent<Rigidbody2D>();
+        springJoint = GetComponent<SpringJoint2D>();
+        rightBranchPos = GameObject.Find("Branch_Right").transform;
     }
 
     private void OnMouseDown()
     {
         isClick = true;
+        rigi.isKinematic = true;
     }
 
 
     private void OnMouseUp()
     {
         isClick = false;
+        rigi.isKinematic = false;
+        Invoke("Fly", 0.1f);
     }
 
     private void Update()
     {
         if (isClick)
         {
-            transform.position = mainCamera.ScreenToWorldPoint(Input.mousePosition+Vector3.forward);
+            transform.position = mainCamera.ScreenToWorldPoint(Input.mousePosition)
+                - new Vector3(0, 0, mainCamera.transform.position.z);
+            if (Vector3.Distance(transform.position, rightBranchPos.position) >= maxDistance)
+            {//进行位置限定
+                Vector3 vec3 = (transform.position - rightBranchPos.position).normalized * maxDistance 
+                    +rightBranchPos.position ;  //单位化*最大长度+初始位置
+                transform.position = vec3;
+            }
+
         }
+    }
+
+    private void Fly()
+    {
+        springJoint.enabled = false;
     }
 }
