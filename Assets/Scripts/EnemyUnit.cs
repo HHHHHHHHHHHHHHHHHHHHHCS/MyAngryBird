5085ab3ab5ef1e311e855aca8da9824f62f16d4d
HCS <464962683@qq.com>
2018-03-12 22:03:56 +0800

    墙体  背景 小草的制作

---
+++ Assets/Scripts/EnemyUnit.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyUnit : MonoBehaviour
+{
+    protected const float sqrMaxSpeed = 64f;//需要根号
+    protected const float sqrMinSpeed = 16f;//需要根号
+
+    [SerializeField]
+    protected Sprite hurtSprite;
+    [SerializeField]
+    protected GameObject score;
+
+
+    protected bool isHurt = false;
+
+
+    protected SpriteRenderer sprite;
+
+    protected virtual void Awake()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    public virtual void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.relativeVelocity.sqrMagnitude > sqrMaxSpeed)
+        {
+            //直接死亡
+            Dead();
+        }
+        else if (collision.relativeVelocity.sqrMagnitude < sqrMinSpeed)
+        {
+            //直接不管
+        }
+        else
+        {
+            if (!isHurt)
+            {
+                //让它受伤
+                sprite.sprite = hurtSprite;
+                isHurt = true;
+            }
+            else
+            {
+                Dead();
+            }
+        }
+    }
+
+    protected virtual void Dead()
+    {
+        Destroy(gameObject);
+        var score = Instantiate(this.score, transform.position + Vector3.up, Quaternion.identity);
+        Destroy(score, 2f);
+    }
+}
--- Assets/Scripts/Pig.cs
+++ Assets/Scripts/Pig.cs
@@ -2,61 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pig : MonoBehaviour
+public class Pig : EnemyUnit
 {
-    private const float sqrMaxSpeed = 64f;//需要根号
-    private const float sqrMinSpeed = 16f;//需要根号
-
     [SerializeField]
-    private Sprite hurtSprite;
-    [SerializeField]
-    private GameObject pigDeadEffect;
-    [SerializeField]
-    private GameObject pigScore;
-
-    private bool isHurt = false;
+    protected GameObject pigDeadEffect;
 
 
-    private SpriteRenderer sprite;
-
-    private void Awake()
+    protected override void Dead()
     {
-        sprite = GetComponent<SpriteRenderer>();
-    }
-
-    public void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.relativeVelocity.sqrMagnitude > sqrMaxSpeed)
-        {
-            //直接死亡
-            Dead();
-        }
-        else if (collision.relativeVelocity.sqrMagnitude < sqrMinSpeed)
-        {
-            //直接不管
-        }
-        else
-        {
-            if(!isHurt)
-            {
-                //让它受伤
-                sprite.sprite = hurtSprite;
-                isHurt = true;
-            }
-            else
-            {
-                Dead();
-            }
-        }
-    }
-
-    private void Dead()
-    {
+        base.Dead();
         var effect = Instantiate(pigDeadEffect, transform.position, Quaternion.identity);
         Destroy(effect, 2f);
-        var score = Instantiate(pigScore, transform.position+Vector3.up, Quaternion.identity);
-        Destroy(score, 2f);
+
         MainGameManager.Instance.RemovePig(this);
-        Destroy(gameObject);
     }
 }
