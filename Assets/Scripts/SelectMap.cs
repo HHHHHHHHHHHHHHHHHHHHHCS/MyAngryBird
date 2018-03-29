b8bccb36d6cf644e83be3fe61ec95634c728af16
HCS <464962683@qq.com>
2018-03-29 22:53:34 +0800

    关卡选择界面 逻辑编写

---
+++ Assets/Scripts/SelectMap.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectMap : MonoBehaviour
+{
+    private int needStar;
+    private int maxStar;
+
+    private int nowStar;
+
+    private GameObject startBg;
+    private GameObject lockBg;
+    private Text starText;
+    private Text needStarText;
+
+    public void SetStarAndLock(int _needStar, int _maxStar)
+    {
+        startBg = transform.Find("StarBg").gameObject;
+        lockBg = transform.Find("LockBg").gameObject;
+        starText = transform.Find("StarBg/StarText").GetComponent<Text>();
+        needStarText = transform.Find("LockBg/NeedStarImage/NeedStarText").GetComponent<Text>();
+
+
+        needStar = _needStar;
+        maxStar = _maxStar;
+        SetLock();
+    }
+
+    private void SetLock()
+    {
+        if (nowStar >= needStar)
+        {
+            GetComponent<Button>().interactable = true;
+            startBg.SetActive(true);
+            lockBg.SetActive(false);
+            SetStarText();
+        }
+        else
+        {
+            GetComponent<Button>().interactable = false;
+            startBg.SetActive(false);
+            lockBg.SetActive(true);
+            needStarText.text = needStar.ToString();
+        }
+    }
+
+    private void SetStarText()
+    {
+        starText.text = string.Format("{0}/{1}", nowStar, maxStar);
+    }
+}
---
+++ Assets/Scripts/SelectMapManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectMapManager : MonoBehaviour
+{
+    private int[,] mapData = new int[7, 2] { { 0,10}, { 5,20}, { 15,30}, { 30,40}, { 50,50}
+        , {75,60 }, { 105,70} };
+    private SelectMap[] selectMaps;
+
+    private void Awake()
+    {
+        selectMaps = transform.GetComponentsInChildren<SelectMap>(true);
+        for(int i= 0;i<selectMaps.Length&&i<mapData.Length;i++)
+        {
+            selectMaps[i].SetStarAndLock(mapData[i,0], mapData[i,1]);
+        }
+    }
+}
